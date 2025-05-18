8796ba83880365759ef51c3c4b8619662e46ad99
Roman Pavlosiuk <rpavlosiuk@gmail.com>
2025-05-18 19:53:38 +0300

    [fix] profile page

--- src/app/components/PokemonCard/PokemonCard.jsx
+++ src/app/components/PokemonCard/PokemonCard.jsx
@@ -1,14 +1,23 @@
 'use client';
 
 import styles from './PokemonCard.module.css';
+import { Star } from 'lucide-react';
 
-export default function PokemonCard({ card }) {
+export default function PokemonCard({ card, onToggleStar }) {
   return (
-    <div className={styles.card}>
+    <div className={styles.cardWrapper}>
       <img src={card.imageUrl} alt={card.name} className={styles.image} />
-      <div className={styles.info}>
-        <strong>{card.name}</strong>
-        <p>Card #{card.id}</p>
+      <div className={styles.footer}>
+        <div className={styles.info}>
+          <strong>{card.name}</strong>
+          <p>Card #{card.id}</p>
+        </div>
+        <button 
+          className={`${styles.favoriteButton} ${card.isStarred ? styles.starred : ''}`}
+          onClick={() => onToggleStar(card.id)}
+        >
+          <Star size={20} fill={card.isStarred ? "currentColor" : "none"} />
+        </button>
       </div>
     </div>
   );
