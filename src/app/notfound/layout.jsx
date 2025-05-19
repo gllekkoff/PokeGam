825771a5ff38713717ec8e2b2ef05964738230f9
Roman Pavlosiuk <rpavlosiuk@gmail.com>
2025-05-19 14:20:36 +0300

    [fix] delete not found page

--- src/app/notfound/layout.jsx
+++
@@ -1,9 +0,0 @@
-import "../globals.css";
-
-export default function NotFoundLayout({ children }) {
-  return (
-    <>
-      {children}
-    </>
-  );
-}
--- src/app/notfound/page.jsx
+++
@@ -1,8 +0,0 @@
-import style from "./styles/notfound.module.css"
-export default function Page() {
-    return (
-      <div className={style.notfound}>
-        {/* Your content here */}
-      </div>
-    );
-  }
